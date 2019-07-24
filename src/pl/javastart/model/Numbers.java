2c296912cfa4ad930b3fa33a4cbd883a65c7740b
adria <Konfitury2>
2019-07-24 10:30:05 +0200

    modyfikacja metod showNumber i showNumbers2

--- src/pl/javastart/model/Numbers.java
+++ src/pl/javastart/model/Numbers.java
@@ -2,24 +2,23 @@ package pl.javastart.model;
 
 public class Numbers {
 
-    public void showNumbers(){
-        int i = 0;
+    public void showNumbers() {
         double number = 0;
-        while(number < 3.1){
-            System.out.printf("%.1f ", number);
-            i++;
+
+        while (number < 3) {
+            System.out.printf("%.1f, ", number);
             number += 0.1;
         }
+        System.out.printf("%.1f", number);
     }
 
-    public void showNumbers2(){
-        int i = 0;
+    public void showNumbers2() {
         double number = 0;
 
         do {
-            System.out.printf("%.1f ", number);
-            i++;
+            System.out.printf("%.1f, ", number);
             number += 0.1;
-        } while (number < 3.1);
+        } while (number < 3);
+        System.out.printf("%.1f", number);
     }
 }
